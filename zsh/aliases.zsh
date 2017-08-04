ba6c1670cdcb2ea04ae421c460ce70daab7c0421
Erki Naumanis <erki.naumanis@gmail.com>
2017-08-04 12:11:11 +0300

    changes added

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias :q='exit'
+alias cdp='cd /home/erki/Projects'
+alias cdd='cd /home/erki/Downloads'
